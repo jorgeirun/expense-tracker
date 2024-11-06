4b05333915a6f9ce2ab86598595c3f4353b63d27
Jorge <jorgemiv2@gmail.com>
2024-11-06 17:18:42 -0300

    Updates

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function ToggleText() {
-  const [isVisible, setIsVisible] = useState(true);
-
+function App() {
   return (
-    <div>
-      <button onClick={() => setIsVisible(!isVisible)}>
-        Toggle Text
-      </button>
-      {isVisible && <p>This text is toggleable</p>}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default ToggleText;
+export default App;
